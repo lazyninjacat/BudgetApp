2b8f443a3ebe4ded829d2f14eebd8ca5234562af
lazyninjacat <matthew.riddett@gmail.com>
2020-02-20 22:30:15 -0800

    Delete item functionality

--- app.js
+++ app.js
@@ -59,6 +59,25 @@ var budgetController = (function() {
       return newItem;
     },
 
+    deleteItem: function(type, id) {
+      var ids, index;
+
+      // id = 3
+      // data.allItems[type][id];
+      // ids = [1 2 3 4 6 8]
+      // index = 3
+
+      ids = data.allItems[type].map(function(current) {
+        return current.id;
+      });
+
+      index = ids.indexOf(id);
+
+      if (index !== -1) {
+        data.allItems[type].splice(index, 1);
+      }
+    },
+
     calculateBudget: function() {
       // calculate total income and expenses
       calculateTotal("exp");
@@ -137,6 +156,11 @@ var UIController = (function() {
 
       // Insert the html into the DOM
       document.querySelector(element).insertAdjacentHTML("beforeend", newHtml);
+    },
+
+    deleteListItem: function(selectorID) {
+      var el = document.getElementById(selectorID);
+      el.parentNode.removeChild(el);
     },
 
     clearFields: function() {
@@ -228,13 +252,16 @@ var controller = (function(budgetCtrl, UICtrl) {
       //inc-1
       splitID = itemID.split("-");
       type = splitID[0];
-      ID = splitID[1];
+      ID = parseInt(splitID[1]);
 
       // 1. delete item from the data structure
+      budgetCtrl.deleteItem(type, ID);
 
       // 2. Delete the item from the UI
+      UICtrl.deleteListItem(itemID);
 
       // 3. Update and show the new budget
+      updateBudget();
     }
   };
 
